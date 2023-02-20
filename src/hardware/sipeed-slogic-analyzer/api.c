a2255a5139b1d36aa2cb97b67cc8c04371fe28c8
taorye <taorye@outlook.com>
2023-02-20 09:43:04 +0800

    sipeed-slogic-analyzer: Add support for SlogicCombo8
    feat: use pattern to control active channels
    
    feat: capture data and regroup channels
    
    feat: now support max 160Msps(2ch)
    
    fix: limit samplerate and auto fit channel and ...
    
    enable to reconnect by rescan and add new samplerates
    
    test for windows
    
    test windows libusb
    
    test for continous and multi channel
    
    test
    
    fix: hardware/sipeed-slogic-analyzer/api.c conn init with NULL instead of UB

--- src/hardware/sipeed-slogic-analyzer/api.c
+++ src/hardware/sipeed-slogic-analyzer/api.c
@@ -20,55 +20,214 @@
 #include <config.h>
 #include "protocol.h"
 
+static const uint32_t scanopts[] = {
+	SR_CONF_CONN,
+};
+
+static const uint32_t drvopts[] = {
+	SR_CONF_LOGIC_ANALYZER,
+};
+
+static const uint32_t devopts[] = {
+	SR_CONF_CONTINUOUS,
+	SR_CONF_LIMIT_SAMPLES | SR_CONF_GET | SR_CONF_SET,
+	SR_CONF_SAMPLERATE    | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+	SR_CONF_TRIGGER_MATCH | SR_CONF_GET | SR_CONF_LIST,
+};
+
+static const int32_t trigger_matches[] = {
+	SR_TRIGGER_ZERO,
+	SR_TRIGGER_ONE,
+	SR_TRIGGER_RISING,
+	SR_TRIGGER_FALLING,
+	SR_TRIGGER_EDGE,
+};
+
 static struct sr_dev_driver sipeed_slogic_analyzer_driver_info;
 
 static GSList *scan(struct sr_dev_driver *di, GSList *options)
 {
+	int ret;
+	struct sr_dev_inst *sdi;
+	struct sr_usb_dev_inst *usb;
 	struct drv_context *drvc;
+	struct dev_context *devc;
+
+	struct sr_config *option;
+	struct libusb_device_descriptor des;
 	GSList *devices;
+	GSList *l, *conn_devices;
+	const char *conn;
+	char cbuf[128];
+	char *iManufacturer, *iProduct, *iSerialNumber, *iPortPath;
 
 	(void)options;
+
+	conn = NULL;
 
 	devices = NULL;
 	drvc = di->context;
 	drvc->instances = NULL;
-
-	/* TODO: scan for devices, either based on a SR_CONF_CONN option
+	
+	/* scan for devices, either based on a SR_CONF_CONN option
 	 * or on a USB scan. */
+	for (l = options; l; l = l->next) {
+		option = l->data;
+		switch (option->key) {
+		case SR_CONF_CONN:
+			conn = g_variant_get_string(option->data, NULL);
+			sr_info("use conn: %s", conn);
+			break;
+		default:
+			sr_warn("Unhandled option key: %u", option->key);
+		}
+	}
+	
+	if(!conn) {
+		conn = "359f.0300";
+	}
 
-	return devices;
+	/* Find all slogic compatible devices. */
+	conn_devices = sr_usb_find(drvc->sr_ctx->libusb_ctx, conn);
+	for(l = conn_devices; l; l = l->next) {
+		usb = l->data;
+		ret = sr_usb_open(drvc->sr_ctx->libusb_ctx, usb);
+		if (SR_OK != ret) continue;
+		libusb_get_device_descriptor(
+			libusb_get_device(usb->devhdl), &des);
+		libusb_get_string_descriptor_ascii(usb->devhdl,
+				des.iManufacturer, cbuf, sizeof(cbuf));
+		iManufacturer = g_strdup(cbuf);
+		libusb_get_string_descriptor_ascii(usb->devhdl,
+				des.iProduct, cbuf, sizeof(cbuf));
+		iProduct = g_strdup(cbuf);
+		libusb_get_string_descriptor_ascii(usb->devhdl,
+				des.iSerialNumber, cbuf, sizeof(cbuf));
+		iSerialNumber = g_strdup(cbuf);
+		usb_get_port_path(libusb_get_device(usb->devhdl),
+				cbuf, sizeof(cbuf));
+		iPortPath = g_strdup(cbuf);
+		sr_usb_close(usb);
+
+		sdi = sr_dev_inst_user_new(iManufacturer, iProduct, NULL);
+		if (!sdi) continue;
+
+		for (int i = 0; i < 8; i++) {
+			sr_snprintf_ascii(cbuf, sizeof(cbuf), "D%d", i);
+			sr_dev_inst_channel_add(sdi, i, SR_CHANNEL_LOGIC, cbuf);
+		}
+		
+		sdi->serial_num = iSerialNumber;
+		sdi->connection_id = iPortPath;
+		sdi->status = SR_ST_INACTIVE;
+		sdi->conn = usb;
+		sdi->inst_type = SR_INST_USB;
+
+		devc = g_malloc0(sizeof(struct dev_context));
+		sdi->priv = devc;
+
+
+
+		devices = g_slist_append(devices, sdi);
+	}
+	// g_slist_free_full(conn_devices, (GDestroyNotify)sr_usb_dev_inst_free);
+
+	return std_scan_complete(di, devices);
 }
 
 static int dev_open(struct sr_dev_inst *sdi)
 {
-	(void)sdi;
+	int ret;
+	struct sr_usb_dev_inst *usb;
+	struct dev_context *devc;
+	struct sr_dev_driver *di;
+	struct drv_context *drvc;
 
+
+	if (!sdi) return SR_ERR_DEV_CLOSED;
 	/* TODO: get handle from sdi->conn and open it. */
+	usb  = sdi->conn;
+	devc = sdi->priv;
+	di	 = sdi->driver;
+	drvc = di->context;
 
-	return SR_OK;
+	ret = sr_usb_open(drvc->sr_ctx->libusb_ctx, usb);
+	if (SR_OK != ret) return ret;
+
+	ret = libusb_claim_interface(usb->devhdl, 0);
+	if (ret != LIBUSB_SUCCESS) {
+		switch (ret) {
+		case LIBUSB_ERROR_BUSY:
+			sr_err("Unable to claim USB interface. Another "
+			       "program or driver has already claimed it.");
+			break;
+		case LIBUSB_ERROR_NO_DEVICE:
+			sr_err("Device has been disconnected.");
+			break;
+		default:
+			sr_err("Unable to claim interface: %s.",
+			       libusb_error_name(ret));
+			break;
+		}
+		return SR_ERR;
+	}
+
+	devc_set_samplerate(devc, samplerates[7]);
+	
+	return std_dummy_dev_open(sdi);
 }
 
 static int dev_close(struct sr_dev_inst *sdi)
 {
-	(void)sdi;
+	int ret;
+	struct sr_usb_dev_inst *usb;
+	struct dev_context *devc;
+	struct sr_dev_driver *di;
+	struct drv_context *drvc;
 
 	/* TODO: get handle from sdi->conn and close it. */
+	usb  = sdi->conn;
+	devc = sdi->priv;
+	di	 = sdi->driver;
+	drvc = di->context;
 
-	return SR_OK;
+	ret = libusb_release_interface(usb->devhdl, 0);
+	if (ret != LIBUSB_SUCCESS) {
+		switch (ret) {
+		case LIBUSB_ERROR_NO_DEVICE:
+			sr_err("Device has been disconnected.");
+			// return SR_ERR_DEV_CLOSED;
+			break;
+		default:
+			sr_err("Unable to release Interface for %s.",
+					libusb_error_name(ret));
+			break;
+		}
+	}
+	
+	sr_usb_close(usb);
+	
+	return std_dummy_dev_close(sdi);
 }
 
 static int config_get(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
 	int ret;
+	struct dev_context *devc;
 
-	(void)sdi;
-	(void)data;
 	(void)cg;
+
+	devc = sdi->priv;
 
 	ret = SR_OK;
 	switch (key) {
-	/* TODO */
+	case SR_CONF_SAMPLERATE:
+		*data = g_variant_new_uint64(devc->cur_samplerate);
+		break;
+	case SR_CONF_LIMIT_SAMPLES:
+		*data = g_variant_new_uint64(devc->limit_samples);
+		break;
 	default:
 		return SR_ERR_NA;
 	}
@@ -80,14 +239,35 @@ static int config_set(uint32_t key, GVariant *data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
 	int ret;
+	struct dev_context *devc;
 
-	(void)sdi;
-	(void)data;
 	(void)cg;
+
+	devc = sdi->priv;
 
 	ret = SR_OK;
 	switch (key) {
-	/* TODO */
+	case SR_CONF_SAMPLERATE:
+		if (std_u64_idx(data, ARRAY_AND_SIZE(samplerates)) < 0) {
+			ret = SR_ERR_ARG;
+		} else {
+			devc_set_samplerate(devc, g_variant_get_uint64(data));
+			{
+			size_t idx = 0;
+				for (GSList *l = sdi->channels; l; l = l->next, idx += 1) {
+					struct sr_channel *ch = l->data;
+					if (ch->type == SR_CHANNEL_LOGIC) { /* Might as well do this now, these are static. */
+						sr_dev_channel_enable(ch, (idx >= devc->cur_samplechannel) ? FALSE : TRUE);
+					} else {
+						return SR_ERR_BUG;
+					}
+				}
+			}
+		}
+		break;
+	case SR_CONF_LIMIT_SAMPLES:
+		devc->limit_samples = g_variant_get_uint64(data);
+		break;
 	default:
 		ret = SR_ERR_NA;
 	}
@@ -100,37 +280,24 @@ static int config_list(uint32_t key, GVariant **data,
 {
 	int ret;
 
-	(void)sdi;
-	(void)data;
-	(void)cg;
-
 	ret = SR_OK;
 	switch (key) {
 	/* TODO */
+	case SR_CONF_SCAN_OPTIONS:
+	case SR_CONF_DEVICE_OPTIONS:
+		ret = STD_CONFIG_LIST(key, data, sdi, cg, scanopts, drvopts, devopts);
+		break;
+	case SR_CONF_SAMPLERATE:
+		*data = std_gvar_samplerates(ARRAY_AND_SIZE(samplerates));
+		break;
+	case SR_CONF_TRIGGER_MATCH:
+		*data = std_gvar_array_i32(ARRAY_AND_SIZE(trigger_matches));
+		break;
 	default:
-		return SR_ERR_NA;
+		ret = SR_ERR_NA;
 	}
 
 	return ret;
-}
-
-static int dev_acquisition_start(const struct sr_dev_inst *sdi)
-{
-	/* TODO: configure hardware, reset acquisition state, set up
-	 * callbacks and send header packet. */
-
-	(void)sdi;
-
-	return SR_OK;
-}
-
-static int dev_acquisition_stop(struct sr_dev_inst *sdi)
-{
-	/* TODO: stop acquisition. */
-
-	(void)sdi;
-
-	return SR_OK;
 }
 
 static struct sr_dev_driver sipeed_slogic_analyzer_driver_info = {
@@ -147,8 +314,8 @@ static struct sr_dev_driver sipeed_slogic_analyzer_driver_info = {
 	.config_list = config_list,
 	.dev_open = dev_open,
 	.dev_close = dev_close,
-	.dev_acquisition_start = dev_acquisition_start,
-	.dev_acquisition_stop = dev_acquisition_stop,
+	.dev_acquisition_start = sipeed_slogic_acquisition_start,
+	.dev_acquisition_stop = sipeed_slogic_acquisition_stop,
 	.context = NULL,
 };
 SR_REGISTER_DEV_DRIVER(sipeed_slogic_analyzer_driver_info);
